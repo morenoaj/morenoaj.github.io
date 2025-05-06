f58672e94db63bb398be6371273fa49cb62fa32b
Alex Javier Moreno <63484452+morenoaj@users.noreply.github.com>
2025-05-06 15:00:44 -0500

    Add deploy configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://morenoaj.github.io",
+  
   integrations: [tailwind(), react()],
   vite: {
     resolve: {
