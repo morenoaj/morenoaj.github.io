091ac360cd66f1f0b17011e2d457ff8a18a19c2b
Alex Javier Moreno <63484452+morenoaj@users.noreply.github.com>
2025-05-07 09:37:27 -0500

    Update template

--- src/React/SkillsList.tsx
+++ src/React/SkillsList.tsx
@@ -61,7 +61,7 @@ const SkillsList = () => {
   return (
     <div className="text-left pt-3 md:pt-9">
       <h3 className="text-[var(--white)] text-3xl md:text-4xl font-semibold md:mb-6">
-        What I do?
+        ¿A qué me dedico?
       </h3>
       <ul className="space-y-4 mt-4 text-lg">
         {Object.entries(skills).map(([category, items]) => (
